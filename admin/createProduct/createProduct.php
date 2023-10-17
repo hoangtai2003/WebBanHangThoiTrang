72d1b0714e729a7bb4f09a61740244c865c91220
cuongkoi <cuongkoi1406@gmail.com>
2023-10-17 10:12:46 +0700

    cuong-10/12

--- admin/createProduct/createProduct.php
+++ admin/createProduct/createProduct.php
@@ -1,14 +1,100 @@
 <?php
 session_start();
 
-    include('../../config/config.php');
-    include('../includes/header.php'); 
-    include_once('../includes/navbar_top.php');
-    include_once('../includes/sidebar.php')
+include('../../config/config.php');
+include('../includes/header.php');
+include_once('../includes/navbar_top.php');
+include_once('../includes/sidebar.php')
 ?>
-   
+<!DOCTYPE html>
+<html>
 
-<?php include('../includes/footer.php');
-?>
-    
+<head>
+    <title>Thêm Sản Phẩm Mới</title>
+   <link rel="stylesheet" href="./createProduct.css">
+</head>
 
+<body>
+<div class="container-new-product">
+        <form class="form-create-product" action="./createproductaction.php" method="post" enctype="multipart/form-data">
+            <span>
+                <div class="form-container">
+                    <h1>Tạo sản phẩm mới</h1>
+                    <label for="product_type_input">Loại hàng:</label>
+                    <input type="text" id="product_type_input" placeholder="Sử dụng gợi ý sẽ dễ dang tiếp cận khách hàng hơn" name="product_type">
+                    <select id="product_type_select" onchange="updateInput()">
+                        <option value="">Gợi ý loại hàng</option>
+                        <?php # for ($i = 0; $i < count($category); $i++) { ?>
+                            <option value="Điện thoại thông minh">Điện thoại thông minh</option>
+                            <option value="Laptop, Máy tính cá nhân">Laptop, Máy tính cá nhân</option>
+                            <option value="Thiết bị gia dụng">Thiết bị gia dụng</option>
+                            <option value="<?php # echo $category[$i]["category"] ?>"><?php # echo $category[$i]["category"] ?></option>
+                        <?php # }   ?>
+                    </select>
+
+                    <script>
+                        const updateInput = () => {
+                            var select = document.getElementById("product_type_select");
+                            var inputCategory = document.getElementById("product_type_input");
+                            inputCategory.value = select.value;
+                        }
+                    </script>
+
+
+                    <label for="product_name">Tên sản phẩm:</label>
+                    <input type="text" placeholder="Tên sản phẩm không quá 255 kí tự" id="product_name" name="product_name">
+
+                    <label for="product_quantity">Số lượng:</label>
+                    <input type="number" id="product_quantity" name="product_quantity">
+
+                    <div class="price-container">
+                        <div class="price-input">
+                            <label for="product_price">Giá gốc:</label>
+                            <input type="number" placeholder="Giá sản phẩm không vượt quá 1 tỷ đồng" id="product_price" name="product_price">
+                        </div>
+                        <div class="price-input">
+                            <label for="product_sale_price">Giá đã được giảm giá:</label>
+                            <input type="number" placeholder="Giá sản phẩm không vượt quá 1 tỷ đồng" id="product_sale_price" name="product_sale_price">
+                        </div>
+                    </div>
+
+                </div>
+
+
+                <div class="upload-container">
+                    <label for="input-img" class="preview">
+                        <i class="fas fa-cloud-upload-alt"></i>
+                        <span>Chọn tệp cho sản phẩm (<1MB JPEG/JPG/PNG)</span>
+                        <input type="file" name="img_post" hidden id="input-img" />
+                        <img alt="" class="img_preview">
+                    </label>
+
+                </div>
+            </span>
+
+            <label for="product_desc">Mô tả:</label>
+            <textarea id="product_desc" name="product_desc"></textarea>
+            <input type="submit" value="Thêm sản phẩm">
+        </form>
+    </div>
+    <script>
+        const inputImg = document.querySelector('#input-img')
+        inputImg.addEventListener('input', (e) => {
+            let file = e.target.files[0]
+            if (!file) return
+            // let img = document.createElement('img')
+            // img.className = "img_preview"
+            document.querySelector(".img_preview").src = URL.createObjectURL(file)
+            document.querySelector("#avt_link_img").value = inputImg.value.substring(inputImg.value.lastIndexOf('\\') + 1);
+            document.querySelector('.preview').appendChild(img)
+        })
+
+        window.onload = function() {
+            openModal();
+        }
+    </script>
+    <?php include('../includes/footer.php');
+    ?>
+</body>
+
+</html>
--- admin/myProduct/myProduct.php
+++ admin/myProduct/myProduct.php
@@ -1,103 +1,42 @@
 <?php
 session_start();
 
-   
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./myProduct.css">
     <title>Document</title>
 </head>
+
 <body>
     <?php
-         include('../../config/config.php');
-         include('../includes/header.php'); 
-         include_once('../includes/navbar_top.php');
-         include_once('../includes/sidebar.php');
-        require_once('../../config/config.php');
-       
+    include('../../config/config.php');
+    include('../includes/header.php');
+    include_once('../includes/navbar_top.php');
+    include_once('../includes/sidebar.php');
+    require_once('../../config/config.php');
+
     ?>
     <!-- container-product -->
     <div class="container">
         <div class="box-product-home box-home">
-            <?php
-            if (count($_REQUEST) > 0 && $_REQUEST["UserId"] != $_SESSION["UserId"]) {
-                echo '<div class="header">
-                        <h4>Cửa hàng của ' . $products[0]['username'] . '</h4>
-                      </div>
-                        <br>
-
-                        <a href="../chatBox/Inbox.php?userID='.$_REQUEST["user_id"].'">
-                            <button class="new-product">
-                            <i class="fa-brands fa-facebook-messenger"></i></i> Chat ngay
-                            </button>
-                        </a>';
-            } else {
-                echo '<div class="header">
-                            <h4>Cửa hàng của tôi</h4>
-                        </div>
-                        <br>
-                        <a href="../createProduct/createproduct.php">
-                            <button class="new-product">
-                                <i class="fa-solid fa-plus"></i> Thêm sản phẩm
-                            </button>
-                        </a>
-                        ';
-            };
-            ?>
-
-
-            <div class="col-content lts-product">
-
-                <?php
-
-                for ($i = count($products) - 1; $i >= 0; $i--) {
-                ?>
-                    <div class="item">
-                        <?php if (count($_REQUEST) <= 0) { ?>
-                            <i class="fa-solid fa-ellipsis-vertical menu_my_product"></i>
-                            <div class="product">
-                                <div class="product-options">
-                                    <span><i class="fa-solid fa-wrench"></i><a href="../UpdateProduct/UpdateProduct.php?id_product=<?php echo $products[$i]["id_product"] ?>">Sửa sản phẩm</a></span>
-                                    <span><i class="fa-sharp fa-solid fa-trash"></i><a href="../DeleteProduct/DeleteProductAction.php?id_product=<?php echo $products[$i]["id_product"] ?>">Xóa sản phẩm</a></span>
-                                </div>
-                            </div>
-                        <?php } ?>
-                        <a href="../ProductDetails/ProductDetailsLayout.php?id_product=<?php echo $products[$i]["id_product"] ?>">
-
-                            <div class="img">
-
-                                <img class="img_product" src="<?php echo   $products[$i]["media_link"] !== NULL ? "../Upload/file_media.php?name=" . $products[$i]["media_link"]  : "https://gaixinhbikini.com/wp-content/uploads/2022/09/52321187927_023da6d2ec_o.jpg" ?>" alt="Ảnh sản phẩm">
-
-                            </div>
-
-
-                            <div class="info">
-                                <a class="title"><?php echo $products[$i]['name_product']; ?></a>
-                                <span class="price">
-                                    <s><strong><?php echo number_format($products[$i]['price_root']); ?>₫</strong></s>
-                                    <strong><?php echo number_format($products[$i]['price']); ?>₫</strong>
-                                </span>
-                            </div>
-
-                            <div class="note">
-
-                                <span class="bag">KM</span>
-                                <label><?php echo $products[$i]['total_sold']; ?> đã bán</label>
-                            </div>
-                        </a>
-
-                    </div>
-                <?php
-                }
-                ?>
-
+            <div class="header">
+                <h4>Cửa hàng của tôi</h4>
             </div>
+            <br>
+            <a href="../createProduct/createproduct.php">
+                <button class="new-product">
+                    <i class="fa-solid fa-plus"></i> Thêm sản phẩm
+                </button>
+            </a>
         </div>
     </div>
-        
+
 </body>
+
 </html>
