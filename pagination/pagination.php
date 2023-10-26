26c89314a77cf79dfbe97ca11d2b90da8a76d160
taiht <Hdt13102k3@gmail.com>
2023-10-26 08:51:32 +0700

    thêm boostrap cho pagination

--- pagination/pagination.php
+++ pagination/pagination.php
@@ -1,31 +1,27 @@
+<nav aria-label="...">
+  <ul class="pagination" style="float: right;">
+    <?php if ($current_page > 1){ ?>
+      <li class="page-item" disabled>
+        <a class="page-link" href="?per_page=<?= $item_per_page ?>&page=<?= $current_page - 1 ?>" aria-label="Previous">
+          <span aria-hidden="true">&laquo;</span>
+          <span class="sr-only">Previous</span>
+        </a>
+      </li>
+    <?php } ?>
 
-<?php 
-if ($current_page > 3){
-    $first_page = 1;
-    ?>
-    <a class="page-item" href="?per_page=<?=$item_per_page?>&page=<?= $first_page ?>">First</a>
-<?php }
-if ($current_page > 1){
-    $prev_page = $current_page - 1;
-    ?>
-    <a class="page-item" href="?per_page=<?=$item_per_page?>&page=<?= $prev_page ?>">Prev</a>
-    <?php }?>
-<?php for($num = 1; $num<=$totalPage; $num++){ ?>
-    <?php if ($num!=$current_page){?>
-        <?php if ($num > $current_page - 3 && $num < $current_page + 3){?>
-            <a class="page-item" href="?per_page=<?=$item_per_page?>&page=<?= $num ?>"><?=$num ?></a>
-        <?php }?>        
-    <?php } else {?>
-        <strong class="current-page page-item"> <?=$num?> </strong>
-        <?php } ?>
-<?php } ?>
-<?php 
-if ($current_page < $totalPage - 1 ){
-    $next_page = $current_page + 1;?>
-    <a class="page-item" href="?per_page=<?=$item_per_page?>&page=<?= $next_page ?>">Next</a>
-<?php }
-if ($current_page < $totalPage - 3){
-    $end_page = $totalPage;
-    ?>
-    <a class="page-item" href="?per_page=<?=$item_per_page?>&page=<?= $end_page ?>">Last</a>
-<?php } ?>
+    <?php for ($num = 1; $num <= $totalPage; $num++){ ?>
+      <li class="page-item <?= $num == $current_page ? 'active' : '' ?>" >
+        <a class="page-link" href="?per_page=<?= $item_per_page ?>&page=<?= $num ?>"><?= $num ?></a>
+      </li>
+    <?php } ?>
+        
+    <?php if ($current_page < $totalPage){ ?>
+      <li class="page-item">
+        <a class="page-link" href="?per_page=<?= $item_per_page ?>&page=<?= $current_page + 1 ?>" aria-label="Next">
+          <span aria-hidden="true">&raquo;</span>
+          <span class="sr-only">Next</span>
+        </a>
+      </li>
+    <?php } ?>
+  </ul>
+</nav>
